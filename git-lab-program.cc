0b3ce80f9895a460b2d84cb82ca6cd00be35e4d9
Samantha Hanson <73858035+samanthahanson75@users.noreply.github.com>
2021-02-01 12:06:21 -0500

    3rd commit

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Samantha Hanson>
+ *        Date: <01-28-21>
+ * Description: #2 lab
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+
+    return 0;
+}// main
